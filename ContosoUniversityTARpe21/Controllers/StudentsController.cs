75e55eeea4b517accace748883d6ca8f46a8a606
Estlib <75094683+Estlib@users.noreply.github.com>
2023-09-05 08:16:17 +0300

    commit

---
+++ ContosoUniversityTARpe21/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using ContosoUniversityTARpe21.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversityTARpe21.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+    }
+}
