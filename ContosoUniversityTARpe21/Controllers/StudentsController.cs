05345461ae27a1c0e24cca94ffb684e7ef54c72b
Estlib <75094683+Estlib@users.noreply.github.com>
2023-09-11 13:33:24 +0300

    08-09

--- ContosoUniversityTARpe21/Controllers/StudentsController.cs
+++ ContosoUniversityTARpe21/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
 ﻿using ContosoUniversityTARpe21.Data;
+using ContosoUniversityTARpe21.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,5 +17,113 @@ namespace ContosoUniversityTARpe21.Controllers
         {
             return View(await _context.Students.ToListAsync());
         }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students
+                .Include(s => s.Enrollments)
+                .ThenInclude(e => e.Course)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null) 
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EnrollmentDate,FirstMidName,LastName")] Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " 
+                    + "Try again, and if the problem persist " 
+                    + "see your system administrator.");
+            }
+            return View(student);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var studentToUpdate = await _context.Students.FirstOrDefaultAsync(s => s.ID == id);
+            if (await TryUpdateModelAsync<Student>(studentToUpdate,"",s=>s.FirstMidName, 
+                s=>s.LastName, s=>s.EnrollmentDate)) 
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes. " + 
+                        "Try again, and if the problem persist " + 
+                        "see your system administrator.");
+                }
+            }
+            return View(studentToUpdate);
+        }
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null) 
+            {
+                return NotFound();
+            }
+            var student = await _context.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(s => s.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Deletion has failed, Please try again, and if the problem persists " 
+                    + "see your system administrator.";
+            }
+            return View(student);
+        }
+        
     }
+    
 }
